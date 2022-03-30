6e257ec8e086a933448fd8363c4c4f57f9f18172
OskarRubensson <oskar231@hotmail.com>
2022-03-30 09:44:06 +0200

    Initial commit

--- src/php/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if( !empty($_POST['firstName']) && !empty($_POST['lastName']) ){
-        $first = $_POST['firstName'];
-        $last = $_POST['lastName'];
-        die($first . ", " . $last . " ");
-    }
-    die("fel");
-?>
