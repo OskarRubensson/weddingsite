b0c2e4e05ec922f1bdf4c25a48bc0ae43b616c0e
OskarRubensson <oskar231@hotmail.com>
2022-03-30 14:52:39 +0200

    Added spinner for OSA form

--- src/partials/Fest.js
+++ src/partials/Fest.js
@@ -22,19 +22,21 @@ function OSAForm() {
   const [info, setInfo] = useState("");
   const [name, setName] = useState("");
   const [allergies, setAllergies] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const submitAccept = () => {
     if (name === "") {
       setInfo("Du måste ange ditt namn för att kunna anmäla dig!");
       return;
     }
-
+    setLoading(true);
     // send post to backend using axios
     axios.post(API_URL, { name, allergies, "arrives": true }).then(res => {
       setName("");
       setAllergies("");
       setInfo(<p className="noStyle green">Tack för din anmälan!</p>);
-    }).catch(err => { alert("Hoppsan, det gick inte att skicka in dina uppgifter. Försök igen!"); console.log(err) });
+      setLoading(false);
+    }).catch(err => { alert("Hoppsan, det gick inte att skicka in dina uppgifter. Försök igen!"); console.log(err); setLoading(false); });
   }
 
   const submitDecline = () => {
@@ -42,48 +44,53 @@ function OSAForm() {
       setInfo("Du måste ange ditt namn för att kunna anmäla dig!");
       return;
     }
-
+    setLoading(true);
     // send post to backend using axios
     axios.post(API_URL, { name, allergies, "arrives": false }).then(res => {
       setName("");
       setAllergies("");
       setInfo(<p className="noStyle green">Tack för din anmälan!</p>);
-    }).catch(err => { alert("Hoppsan, det gick inte att skicka in dina uppgifter. Försök igen!"); console.log(err) });
+      setLoading(false);
+    }).catch(err => { alert("Hoppsan, det gick inte att skicka in dina uppgifter. Försök igen!"); console.log(err); setLoading(false); });
   }
 
   return (
     <table style={{padding: "10px 0 40px 0"}}>
-      <tbody>
-        <tr>
-          <td>
-          <b>Namn: </b>&nbsp;
-            <input 
-              type="text"
-              name="name"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </td>
-          <td>
-          <b>Ev. allergier: </b>&nbsp;
-            <input 
-              type="text"
-              name="allergies"
-              value={allergies}
-              onChange={(event) => setAllergies(event.target.value)}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button onClick={submitAccept} className="green-btn">Kommer</button>
-          </td>
-          <td>
-            <button onClick={submitDecline} className="red-btn">Kommer inte</button>
-          </td>
-        </tr>
-        { info ? <tr><td colSpan="2"  style={{color: "rgba(255, 0, 0, 0.9)"}}>{info}</td></tr> : null }
-      </tbody>
+      { loading ? <p className="noStyle">Skickar in din anmälan...</p> : 
+        (
+          <tbody>
+            <tr>
+              <td>
+              <b>Namn: </b>&nbsp;
+                <input 
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                />
+              </td>
+              <td>
+              <b>Ev. allergier: </b>&nbsp;
+                <input 
+                  type="text"
+                  name="allergies"
+                  value={allergies}
+                  onChange={(event) => setAllergies(event.target.value)}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button onClick={submitAccept} className="green-btn">Kommer</button>
+              </td>
+              <td>
+                <button onClick={submitDecline} className="red-btn">Kommer inte</button>
+              </td>
+            </tr>
+            { info ? <tr><td colSpan="2"  style={{color: "rgba(255, 0, 0, 0.9)"}}>{info}</td></tr> : null }
+          </tbody>
+        ) 
+      }
     </table>
   )
 }
